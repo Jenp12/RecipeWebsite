0464ab59dfc2e4adaebcc7cec3d089ff812ea267
Jenp12 <112375918+Jenp12@users.noreply.github.com>
2024-12-12 01:56:23 -0800

    Update hug.bash

--- hug.bash
+++ hug.bash
@@ -1,5 +1,5 @@
 curl -X POST \
-    -H "Authorization: Bearer YOUR_HUGGING_FACE_TOKEN" \
+    -H "Authorization: Bearer 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" \
     -H "Content-Type: application/json" \
     -d '{"inputs":"How do I make lasagna?"}' \
     https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill
