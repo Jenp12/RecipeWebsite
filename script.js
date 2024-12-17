78e5fad8c1fb6b0921fb14a57e955327fa769e4a
Jenp12 <112375918+Jenp12@users.noreply.github.com>
2024-12-16 20:57:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ document.getElementById("signup-form")?.addEventListener("submit", function (e) 
     e.preventDefault();
 
     const username = document.getElementById("username").value.trim();
-    const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
 
